6251531c6ef30d8499ec6f3be7328c74b1c519e1
= <nicolas.papon@webflows.fr>
2010-09-20 10:26:45 +0200

    refactoring des vues

--- lib/generators/itkin/scaffold_controller/scaffold_controller_generator.rb
+++ lib/generators/itkin/scaffold_controller/scaffold_controller_generator.rb
@@ -2,6 +2,7 @@ require 'rails/generators/rails/scaffold_controller/scaffold_controller_generato
 
 module Itkin
   class ScaffoldControllerGenerator < Rails::Generators::ScaffoldControllerGenerator
+
     source_root File.expand_path('../templates', __FILE__)
 
     remove_hook_for :template_engine
