d59781296526974fae58f51618f7e46abf37f41b
= <nicolas.papon@webflows.fr>
2010-09-20 10:44:52 +0200

    add helper methods

--- lib/generators/itkin/haml/haml_generator.rb
+++ lib/generators/itkin/haml/haml_generator.rb
@@ -15,5 +15,29 @@ module Itkin
         directory '.', File.join("app/views", controller_file_path)
       end
 
+      def add_helpers
+        append_file 'app/helpers/application_helper' do
+          <<-STR
+          def flash_message
+            if not flash[:notice].blank?
+              content_tag :div, flash[:notice], :class => 'flash notice'
+            elsif not flash[:error].blank?
+              content_tag :div, flash[:notice], :class => 'flash error'
+            end
+          end
+
+          def update_ajax_multipart_item(instance)
+            instance_name = instance.class.name.downcase
+            raw <<-END
+            var $elt = $('##{instance_name}_#{instance.id}')\;
+            $elt.find('.header-content').html($('#{render_js :partial => 'item', :locals => { instance_name.to_sym => instance }}').children().first().html())\;
+            $elt.children().last().html('#{render_js(:partial => instance)}')\;
+            $('.#{instance_name.pluralize}').ajaxAccordion('activate', $('##{instance_name}_#{instance.id}').index())\;
+            END
+          end
+          STR
+        end
+      end
+
     end
 end
