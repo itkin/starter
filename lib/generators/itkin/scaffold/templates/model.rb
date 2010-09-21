39a89e982591d0a7132fc5591b7ade46089cc5a0
= <nicolas.papon@webflows.fr>
2010-09-21 15:34:43 +0200

    ajout d'une methode de classe search au modele

--- lib/generators/itkin/scaffold/templates/model.rb
+++ lib/generators/itkin/scaffold/templates/model.rb
@@ -2,4 +2,8 @@ class <%= class_name %> < <%= parent_class_name.classify %>
 <% attributes.select {|attr| attr.reference? }.each do |attribute| -%>
   belongs_to :<%= attribute.name %>
 <% end -%>
+
+  def self.search(params)
+    where(params)
+  end
 end
