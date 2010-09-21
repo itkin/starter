55d3b3d686167f20d6a415945cdc6cb249d41197
= <nicolas.papon@webflows.fr>
2010-09-21 15:17:27 +0200

    prise en compte des namespace

--- lib/generators/itkin/scaffold/scaffold_generator.rb
+++ lib/generators/itkin/scaffold/scaffold_generator.rb
@@ -1,4 +1,10 @@
-require 'rails/generators/rails/scaffold/scaffold_generator'
+require 'ruby-debug'
+Debugger.start
+if Debugger.respond_to?(:settings)
+  Debugger.settings[:autoeval] = true
+  Debugger.settings[:autolist] = 1
+end
+
 
 module Itkin
   class ScaffoldGenerator < Rails::Generators::NamedBase
@@ -15,13 +21,25 @@ module Itkin
     class_option :parent,     :type => :string, :desc => "The parent class for the generated model"
 
     def create_migration_file
-      return unless options[:migration] && options[:parent].nil?
+      return unless options[:orm] and options[:migration] && options[:parent].nil?
       migration_template "migration.rb", "db/migrate/create_#{table_name}.rb"
     end
 
     def create_model_file
-      template 'model.rb', File.join('app/models', class_path, "#{file_name}.rb")
+      return unless options[:orm]
+      template 'model.rb', File.join('app/models',  "#{file_name}.rb")
     end
+
+    def create_fixtures_file
+      return unless options[:orm]
+      template 'test/fixtures.yml', File.join('test/fixtures', "#{file_name.pluralize}.yml")
+    end
+
+    def create_unit_test_file
+      return unless options[:orm]
+      template 'test/unit.rb', File.join('test/unit', "#{file_name}_test.rb")
+    end
+
 
     def create_controller_file
       template 'controller.rb', File.join('app/controllers', class_path, "#{controller_file_name}_controller.rb")
@@ -31,17 +49,27 @@ module Itkin
       template 'helper.rb', File.join('app/helpers', class_path, "#{controller_file_name}_helper.rb")
     end
 
-    def create_unit_test_file
-      template 'test/unit.rb', File.join('test/unit', "#{file_name}_test.rb")
-    end
-
     def create_functional_test_file
-      template 'test/functional.rb', File.join('test/functional', class_path, "#{file_name}_test.rb")
+      template 'test/functional.rb', File.join('test/functional', class_path, "#{controller_file_name}_controller_test.rb")
     end
 
-    def create_fixtures_file
-      template 'test/fixtures.yml', File.join('test/fixtures', "#{file_name.pluralize}.yml")
-    end
+    protected
+
+      def parent_class_name
+        options[:parent] || "ActiveRecord::Base"
+      end
+      def assign_names!(name) #:nodoc:
+        super
+        @class_name = file_name.camelize
+        @class_name_with_namespace = (class_path + [file_name]).map!{ |m| m.camelize }.join('::')
+      end
+      def class_name
+        @class_name
+      end
+      def class_name_with_namespace
+        @class_name_with_namespace
+      end
+
 
 
   end
